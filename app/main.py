e2c2d94938f6c8fb6c506137cdd96e598e3f4fb6
DavidWard98 <david.ae.ward@gmail.com>
2022-08-29 20:27:02 -0600

    add strawberry

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from app.modules.example_module.routes import examples
-from .core.routing import GraphQLRouter
-import strawberry
-
 
 app = FastAPI()
 app.include_router(examples.router)
